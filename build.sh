74cea641cbdc4c38e3aa0646958dc9f16076162a
ermyltsor <lamiimuk@163.com>
2025-07-05 00:20:42 +0800

    build.sh: Add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+# Download zyc clang 10.0.1, if needed
+#
+
+KERNEL_DEFCONFIG=vendor/kona-perf_defconfig
+DIR=$PWD
+export ARCH=arm64
+export CLANG_PATH="/$HOME/clang/bin"
+export PATH="$CLANG_PATH:$PATH"
+export CROSS_COMPILE=aarch64-linux-gnu-
+
+echo
+echo "Kernel is going to be built using $KERNEL_DEFCONFIG"
+echo
+
+make CC=clang AR=llvm-ar NM=llvm-nm OBJDUMP=llvm-objdump STRIP=llvm-strip O=out $KERNEL_DEFCONFIG
+
+make CC=clang AR=llvm-ar NM=llvm-nm OBJDUMP=llvm-objdump STRIP=llvm-strip O=out -j$(nproc --all)
