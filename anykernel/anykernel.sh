e09647d2713f851f37b5b474c9a86ec1b77bb5b2
ermyltsor <lamiimuk@163.com>
2025-08-22 11:28:58 +0800

    treewide: Path AnyKernel3 into kernel source source: https://github.com/osm0sis/AnyKernel3.
    Change-Id: I7fda5f71012d7ab56960b1c814a4cff971005b73

---
+++ anykernel/anykernel.sh
@@ -0,0 +1,36 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=Flashing Kernel Now...
+do.devicecheck=0
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+supported.vendorpatchlevels=
+'; } # end properties
+
+# boot shell variables
+BLOCK=boot;
+IS_SLOT_DEVICE=0;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
